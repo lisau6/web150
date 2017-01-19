4ae9e77ab55c2e04e1a57814b4344589e2add68e
Lisa Uebel <Lisa@Lisas-MacBook-Pro.local>
2017-01-18 18:59:39 -0800

    My First Homework

--- 1_variables_expressions/script.js
+++ 1_variables_expressions/script.js
@@ -1,4 +1,6 @@
-var firstName = "Lisa"
-var lastName = "Uebel"
-var yearsOfStudy = "1 week"
-var goal = "become more technical"
+var firstName = "Lisa";
+var lastName = "Uebel";
+var yearsOfStudy = "1 week";
+var goal = "become more technical";
+
+document.write("My name is " + firstName +" "+ lastName + ". I have been at SCC for " + yearsOfStudy + ", and plan to " + goal + " after completing my courses.");
